f65432a9bdb603b8799fcc119f7d515c60a52e43
Andrew Lopatin <andriy.lopatin@neadevis.com>
2015-11-12 15:10:16 +0200

    Added comments to ReportType.cs

--- FastSms/Common/ReportType.cs
+++ FastSms/Common/ReportType.cs
@@ -1,8 +1,26 @@
 ﻿namespace FastSms.Common {
+	/// <summary>
+	///    Report type.
+	/// </summary>
 	public enum ReportType {
+		/// <summary>
+		///    Messages report.
+		/// </summary>
 		Messages,
+
+		/// <summary>
+		///    Outbox report.
+		/// </summary>
 		Outbox,
+
+		/// <summary>
+		///    Usage report.
+		/// </summary>
 		Usage,
+
+		/// <summary>
+		///    Inbound messages report.
+		/// </summary>
 		InboundMessages
 	}
 }
